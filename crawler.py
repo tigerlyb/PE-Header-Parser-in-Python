170653ccac735ea9a66b4ab2c205c1b4fea606d1
Yibin Liao <tigerlyb@gmail.com>
2013-02-25 13:26:26 -0500

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,24 @@
+import re
+import urllib2
+import webbrowser
+import time
+from bs4 import BeautifulSoup
+from appscript import *
+
+downloadLinks = 0
+
+
+for num in range(1500,2000):
+    u = "http://download.cnet.com/windows/3150-20_4-0-" + str(num) + ".html?sort=reviewDate%20asc&tag=page"
+    page = urllib2.urlopen(u)
+    soup = BeautifulSoup(page)
+    
+    for link in soup.findAll(href=re.compile("26dlm")):
+        downloadLinks = downloadLinks + 1
+        print "Number of download links: ", downloadLinks
+        url = link.get('href')
+        print(url)               
+        webbrowser.open(url)
+        time.sleep(10)
+        safari = app("Safari")
+        safari.windows.first.current_tab.close()
